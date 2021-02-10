f74fe201f0e614d92b7baa280b44e68da1d48ec9
Ruan Miguel <ruanmiguelps@gmail.com>
2021-02-10 14:50:58 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderwithrouter';
+
+it('Deve testar se a pagina vai para o notFound', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/pagina/que-nao-existe/');
+  const noMatch = screen.getByRole('heading',
+    { level: 2, name: /page requested not found Crying emoji/i });
+
+  const picture = screen.getByAltText(
+    /pikachu crying because the page requested was not found/i,
+  );
+  expect(picture).toBeInTheDocument();
+  expect(picture.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  expect(noMatch).toBeInTheDocument();
+});
